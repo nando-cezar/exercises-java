e6792920620a445f570f5fdc506d17869cbdf6b3
veyson <luisfernando_cezar@hotmail.com>
2020-10-30 18:27:28 -0300

    Exercicies | completed AcessDB

--- AcessDB/test/TesteUsuarioDAO.java
+++ AcessDB/test/TesteUsuarioDAO.java
@@ -4,7 +4,10 @@
  * and open the template in the editor.
  */
 import java.util.List;
+import org.dbunit.Assertion;
 import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
 import org.dbunit.util.fileloader.FlatXmlDataFileLoader;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -15,28 +18,46 @@ import org.junit.Before;
  * @author Windows
  */
 public class TesteUsuarioDAO {
-    
+
     JdbcDatabaseTester jdt;
-    
+
     public TesteUsuarioDAO() {
     }
-    
+
     @Before
     public void setUp() throws Exception {
-        
-        jdt = new JdbcDatabaseTester("com.mysql.cj.jdbc.Driver", 
-                "jdbc:mysql://localhost:3306/teste?useTimezone=true&serverTimezone=America/Sao_Paulo&useSSL=false", 
+
+        jdt = new JdbcDatabaseTester("com.mysql.cj.jdbc.Driver",
+                "jdbc:mysql://localhost:3306/teste?useTimezone=true&serverTimezone=America/Sao_Paulo&useSSL=false",
                 "root", "Pa$$w0rd");
         FlatXmlDataFileLoader loader = new FlatXmlDataFileLoader();
         jdt.setDataSet(loader.load("/inicio.xml"));
         jdt.onSetup();
     }
-    
+
     @Test
-    public void getAll() {
+    public void recoverAll() {
         List<Usuario> list = UsuarioDAO.getAll();
-        assertEquals(2, list.size());
-        assertEquals("teste1", list.get(0).getLogin());
+        assertEquals(1, list.size());
+        assertEquals("Fernando123", list.get(0).getLogin());
     }
-    
+
+    @Test
+    public void insertNew() throws Exception {
+
+        Usuario usuario = new Usuario();
+        usuario.setLogin("Lucas123");
+        usuario.setNome("Lucas");
+        usuario.setEmail("lucas@teste.com.br");
+        
+        UsuarioDAO.Create(usuario);
+        
+        IDataSet currentDataSet = jdt.getConnection().createDataSet();
+        ITable currentTable = currentDataSet.getTable("USUARIO");      
+        FlatXmlDataFileLoader loader = new FlatXmlDataFileLoader();
+        IDataSet expectedDataSet = loader.load("/verifica.xml");
+        ITable expectedTable = expectedDataSet.getTable("USUARIO");   
+        Assertion.assertEquals(expectedTable, currentTable);
+    }
+
 }
